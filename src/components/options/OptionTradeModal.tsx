fb793ebd7246f4d29c6cd4ab36d3e5b7455cb3b0
Josh Bajorek <milk18001955@gmail.com>
2025-06-15 23:20:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/options/OptionTradeModal.tsx
+++ src/components/options/OptionTradeModal.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import {
   Dialog,
   DialogContent,
@@ -190,3 +190,4 @@ export function OptionTradeModal({
     </Dialog>
   );
 }
+
