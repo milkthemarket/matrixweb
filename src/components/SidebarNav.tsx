ad6e22724ab20857fed98b5d7b7b7ca8a2bff9ec
milkthemarket <info@milkthemarket.io>
2025-07-11 22:07:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -79,7 +79,6 @@ export function SidebarNav() {
         <SidebarMenuButton asChild className="w-full hover:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0">
             <Link href="/dashboard" className="group flex items-center gap-2 min-w-0 hover:opacity-80 transition-opacity duration-150">
                 <CowIcon size={28} className="text-white flex-shrink-0" />
-                {state === 'expanded' && <span className="font-bold text-lg text-white">MILK</span>}
             </Link>
         </SidebarMenuButton>
         {!isMobile && (
